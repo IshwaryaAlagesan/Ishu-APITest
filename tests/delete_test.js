22f71523a0524111e88ff0a5e16ab08a565a65ce
Gopinathan <gopi@Gopinathans-MacBook-Pro.local>
2020-02-16 19:49:54 +0000

    changes

--- tests/delete_test.js
+++ tests/delete_test.js
@@ -12,5 +12,5 @@ Scenario('Verify a deleting the entry successful call - STATUS', async () => {
 Scenario('Verify invalid deleting unsuccessful call - STATUS', async () => {
 	const res = await I.sendDeleteRequest('/product/10');
      assert.equal(404,res.status, 'Status code actual value ' + res.status + ' is not matching with Expected value 200. Please check the Service.');
-    
+    // TEST
 });
